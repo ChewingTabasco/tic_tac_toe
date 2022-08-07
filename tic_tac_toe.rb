f09e06e260895781087ce71fa3d2f0adea08c7ad
ChewingTabasco <emerickjustice@gmail.com>
2022-08-07 15:55:45 -0400

    Refactor default grid layout

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -11,61 +11,48 @@ class Player
     self.name = gets.chomp
 
     puts "#{self.name}, what character would you like to use?"
-    self.char = gets.chomp
+    self.char = gets.chomp.strip[0] #first character of input w/o whitespace
+  end
+
+  def make_move(space)
+    @space = space
+
+    puts "grid number #{@space} should be #{@char}"
   end
 end
 
 class Board
-  def initialize(array)
-    # @top = top
-    # @middle = middle
-    # @bottom = bottom
-    # @bar = bar
-    @array = array
+  def initialize(board_arr, spaces_arr)
+    @board_arr = board_arr
+    @spaces_arr = spaces_arr
   end
 
   def display_board
-    # puts @top.join("")
-    # puts @bar.join("")
-    # puts @middle.join("")
-    # puts @bar.join("")
-    # puts @bottom.join("")
-
-    puts @array
+    puts @board_arr
   end
 end
 
-# one = [" ", 1, " ", "|"," ", 2, " ", "|", " ", 3, " "]
-# two = [" ", 4, " ", "|"," ", 5, " ", "|", " ", 6, " "]
-# three = [" ", 7, " ", "|"," ", 8, " ", "|", " ", 9, " "]
-# bars = ["---+---+---"]
+default_grid_spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 default_board_strings = [
   "     |     |     ", 
-  "  1  |  2  |  3  ", 
+  "  #{default_grid_spaces[0]}  |  #{default_grid_spaces[1]}  |  #{default_grid_spaces[2]}  ", 
   "_____|_____|_____", 
   "     |     |     ", 
-  "  4  |  5  |  6  ", 
+  "  #{default_grid_spaces[3]}  |  #{default_grid_spaces[4]}  |  #{default_grid_spaces[5]}  ", 
   "_____|_____|_____", 
   "     |     |     ", 
-  "  7  |  8  |  9  ", 
+  "  #{default_grid_spaces[6]}  |  #{default_grid_spaces[7]}  |  #{default_grid_spaces[8]}  ", 
   "     |     |     "]
 
-# player1 = Player.new()
-# player1.set_attributes(1)
+player1 = Player.new()
+player1.set_attributes(1)
 
-# player2 = Player.new()
-# player2.set_attributes(2)
+player2 = Player.new()
+player2.set_attributes(2)
 
-# board = Board.new(one, two, three, bars)
-board = Board.new(board_strings)
+board = Board.new(default_board_strings, default_grid_spaces)
 board.display_board
 
-# my_strings = board_strings
-# board_strings.each do |el|
-#   if el.include?("2")
-#     my_strings[1] = my_strings[1].sub("2", "x")
-#   end
-# end
-# board = Board.new(my_strings)
-# board.display_board
+puts "player 1 move?"
+player1.make_move(gets.chomp.to_i)
